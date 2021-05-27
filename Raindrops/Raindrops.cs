31c4ce8bea1f7edbfcbbd91b1faf2aba35d75a05
Jack Dewhirst <jackdewhirst@gmail.com>
2021-05-27 21:46:52 +0100

    Add Raindrops Solution and RaindropsLibrary Project including stub of the Raindrops function

---
+++ Raindrops/Raindrops.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Raindrops
+{
+    public class RaindropsClassLibrary
+    {
+        public string Raindrops(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
